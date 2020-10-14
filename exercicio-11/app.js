0b391b3bdfa3629da8008fb86ad679931c5dacbb
ednaldo.brito <ednaldo.brito@agencian1.com.br>
2020-10-14 00:01:30 -0300

    fazendo exercicios 11

--- exercicio-11/app.js
+++ exercicio-11/app.js
@@ -1,3 +1,26 @@
+let user = {
+  name: 'João', 
+  age: 31, 
+  email: 'joão@gmail.com',
+  city: 'São Paulo',
+  blogPosts: ['Empadão de frango', '4 receitas de purê de batata'],
+  login () {
+    console.log('Usuário logado') 
+  },
+  logout () {
+    console.log('Usuário deslogado')
+  },
+  logBlogPosts () {
+    console.log(`${this.name} escreveu os seguintes posts:`)
+    
+    this.blogPosts.forEach(post => {
+      console.log(post)
+    }) 
+  }
+}
+
+user.logBlogPosts()
+
 /*
   01
 
