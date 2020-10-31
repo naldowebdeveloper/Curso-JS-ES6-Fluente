65b6a5e87af0db364657b7812c3bd457b18d2c71
ednaldo.brito <ednaldo.brito@agencian1.com.br>
2020-10-31 17:56:13 -0300

    fazendo exercio 16

---
+++ exercicio-16/pratica-01.js
@@ -0,0 +1,18 @@
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    const li = document.createElement('li')
+    
+    li.textContent = 'Novo item'
+    ul.prepend(li)
+})
+
+ul.addEventListener('click', event => {
+    const clickedElement = event.target
+    //console.log(event)
+    
+    if(clickedElement.tagName === 'LI'){
+        clickedElement.remove()
+    }
+})
