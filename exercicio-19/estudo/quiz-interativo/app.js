3933a0866e37110080d773d86bff94deb1a05231
ednaldo-brito <ednaldo.brito@agencian1.com.br>
2020-12-26 14:30:34 -0300

    setTimeout

--- exercicio-19/estudo/quiz-interativo/app.js
+++ exercicio-19/estudo/quiz-interativo/app.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
 
 const correctAnswers = ['B', 'B', 'B', 'B']
 
@@ -11,12 +12,28 @@ form.addEventListener('submit', event => {
         form.inputQuestion2.value,
         form.inputQuestion3.value,
         form.inputQuestion4.value,
-    ]
+    ] 
 
     userAnswers.forEach((userAnswer, index) => {
         if(userAnswer === correctAnswers[index]){
             score += 25
         }
     })
-    console.log(score)
+
+    scrollTo(0, 0)
+
+    finalResult.classList.remove('d-none')
+    
+    let counter = 0
+
+    const timer = setInterval(() => {
+        if (counter === score) {
+            clearInterval(timer)
+        }
+
+        finalResult.querySelector('span').textContent = `${counter}%`
+        counter++
+    }, 10)  
 })
+
+
